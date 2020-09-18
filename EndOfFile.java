640ed761e2cbe8f0effd77e3b8bcf2e98b9915c9
Akila De Silva <54980465+AkilaDee@users.noreply.github.com>
2020-09-19 00:08:49 +0530

    Create EndOfFile.java

---
+++ EndOfFile.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        int i=0;
+        Scanner str = new Scanner(System.in);
+       
+        while(str.hasNext()){
+            i++;
+        
+         String s1 = str.nextLine();
+        System.out.println(i+" "+s1);
+        }
+    }
+}
